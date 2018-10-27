69194ec4e0e18699933fec8582f0d952ab8cef3a
Assem Chelli <assem.ch@gmail.com>
2018-10-27 06:36:27 +0100

    change message.text to TextField

--- bot/models.py
+++ bot/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class Message(models.Model):
-    text = models.CharField(max_length=200)
+    text = models.TextField()
     frequency = models.PositiveIntegerField(default=0)
     blacklist = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
