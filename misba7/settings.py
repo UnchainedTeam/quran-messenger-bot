c49bb184f2d2a8fa2d128b503f255d0693122074
Assem Chelli <assem.ch@gmail.com>
2018-10-27 07:41:44 +0100

    fix static

--- misba7/settings.py
+++ misba7/settings.py
@@ -129,11 +129,7 @@ USE_TZ = True
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 MEDIA_URL = '/media/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, "static_files/")
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_URL = '/static/'
 
-
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
