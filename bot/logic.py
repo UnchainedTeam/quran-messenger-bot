2a893ee9007bb032aceb152ef434a2e7e3ebe156
Assem Chelli <assem.ch@gmail.com>
2018-10-27 07:19:16 +0100

    fix problem in results

--- bot/logic.py
+++ bot/logic.py
@@ -3,13 +3,14 @@
 import alfanous
 
 def answer(query):
-    limit = 3
+    limit = 4
     response = alfanous.do(flags={"action":"search", "query":query, "unit": "aya", "fuzzy":True, "highlight": "none", "limit": limit})
     results=[]
     if (not response["error"]["code"]):
         for i in xrange(1,  min(limit+1, response["search"]["interval"]["total"]+1)):
-             result = {
+            result = {
                 'title':  "{" + response["search"]["ayas"][i]["identifier"]["sura_arabic_name"] + " "+ str(response["search"]["ayas"][i]["identifier"]["aya_id"]) + "}",
                 'body': "\n"+ response["search"]["ayas"][i]["aya"]["text"]
              }
+            results.append(result)
     return results
